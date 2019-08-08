9fed86b20b57642335ff49b693f10de58edcb4c1
vasi <vasi@exemplifai.io>
2019-08-08 18:30:31 +0530

    ecomodule

---
+++ src/components/ecosystem/index.js
@@ -0,0 +1,68 @@
+import React from 'react'
+import { Link } from 'gatsby'
+// Import custom style for this component
+//import '../Ecosystem/style.scss'
+import "./ecosystem.css"
+const Ecosystem = () => (
+  <div class="eco-container">
+  <ul class="portfolio-filters">
+		<li class="portfolio-filter active">All</li>
+		<li class="portfolio-filter">Transportation</li>
+		<li class="portfolio-filter">Agriculture</li>
+		<li class="portfolio-filter">Manufacturing</li>
+		<li class="portfolio-filter">Technology</li>
+		<li class="portfolio-filter">Telecom</li>
+		<li class="portfolio-filter">Government</li>
+		<li class="portfolio-filter">Education</li>
+		<li class="portfolio-filter">NGO</li>
+		<li class="portfolio-filter">Consumer</li>
+		<li class="portfolio-filter">Automotive</li>
+		<li class="portfolio-filter">Pharma</li>
+		<li class="portfolio-filter">B2B</li>
+		<li class="portfolio-filter">Insurance</li>
+		<li class="portfolio-filter">Financial Services</li>
+		<li class="portfolio-filter">Healthcare</li>
+	</ul>
+  <div class="filter-block-wrap">
+  <div class="filter-block-content portfolio-filter-block">
+    <img src="https://1c56ea1o8ppe3ily2evtgp8h-wpengine.netdna-ssl.com/wp-content/uploads/2019/05/Mautic-Logo.png" alt=""/>
+  </div>
+  <div class="filter-block-content portfolio-filter-block">
+    <img src="https://1c56ea1o8ppe3ily2evtgp8h-wpengine.netdna-ssl.com/wp-content/uploads/2019/05/Hubspot-Logo.png" alt=""/>
+  </div>
+  <div class="filter-block-content portfolio-filter-block">
+    <img src="https://1c56ea1o8ppe3ily2evtgp8h-wpengine.netdna-ssl.com/wp-content/uploads/2019/05/Marketo-Logo-circle.png" alt=""/>
+  </div>
+  <div class="filter-block-content portfolio-filter-block">
+    <img src="https://1c56ea1o8ppe3ily2evtgp8h-wpengine.netdna-ssl.com/wp-content/uploads/2019/05/Pardot-Logo.png" alt=""/>
+  </div>
+  <div class="filter-block-content portfolio-filter-block">
+    <img src="https://1c56ea1o8ppe3ily2evtgp8h-wpengine.netdna-ssl.com/wp-content/uploads/2019/05/Mailchimp-Logo.png" alt=""/>
+  </div>
+  <div class="filter-block-content portfolio-filter-block">
+    <img src="https://1c56ea1o8ppe3ily2evtgp8h-wpengine.netdna-ssl.com/wp-content/uploads/2019/05/Adobe-campaign-logo.png" alt=""/>
+  </div>
+  
+  
+  <div class="filter-block-content portfolio-filter-block">
+    <img src="https://1c56ea1o8ppe3ily2evtgp8h-wpengine.netdna-ssl.com/wp-content/uploads/2019/05/Mautic-Logo.png" alt=""/>
+  </div>
+  <div class="filter-block-content portfolio-filter-block">
+    <img src="https://1c56ea1o8ppe3ily2evtgp8h-wpengine.netdna-ssl.com/wp-content/uploads/2019/05/Hubspot-Logo.png" alt=""/>
+  </div>
+  <div class="filter-block-content portfolio-filter-block">
+    <img src="https://1c56ea1o8ppe3ily2evtgp8h-wpengine.netdna-ssl.com/wp-content/uploads/2019/05/Marketo-Logo-circle.png" alt=""/>
+  </div>
+  <div class="filter-block-content portfolio-filter-block">
+    <img src="https://1c56ea1o8ppe3ily2evtgp8h-wpengine.netdna-ssl.com/wp-content/uploads/2019/05/Pardot-Logo.png" alt=""/>
+  </div>
+  <div class="filter-block-content portfolio-filter-block">
+    <img src="https://1c56ea1o8ppe3ily2evtgp8h-wpengine.netdna-ssl.com/wp-content/uploads/2019/05/Mailchimp-Logo.png" alt=""/>
+  </div>
+  <div class="filter-block-content portfolio-filter-block">
+    <img src="https://1c56ea1o8ppe3ily2evtgp8h-wpengine.netdna-ssl.com/wp-content/uploads/2019/05/Adobe-campaign-logo.png" alt=""/>
+  </div>
+</div></div>
+)
+
+export default Ecosystem
