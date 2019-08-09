dc46e59a106958ef34f277d209346a9071b43e57
vasi <vasi@exemplifai.io>
2019-08-09 18:59:06 +0530

    eco

--- src/components/ecosystem/index.js
+++ src/components/ecosystem/index.js
@@ -3,50 +3,29 @@ import { Link } from 'gatsby'
 // Import custom style for this component
 //import '../Ecosystem/style.scss'
 import "./ecosystem.css"
-const Ecosystem = () => (
+const Ecosystem = (props) => (
   <div class="eco-container">
   <ul class="portfolio-filters">
-		<li class="portfolio-filter active">All</li>
-		<li class="portfolio-filter">Transportation</li>
-		<li class="portfolio-filter">Agriculture</li>
-		<li class="portfolio-filter">Manufacturing</li>
-		<li class="portfolio-filter">Technology</li>
-		<li class="portfolio-filter">Telecom</li>
-		<li class="portfolio-filter">Government</li>
-		<li class="portfolio-filter">Education</li>
-		<li class="portfolio-filter">NGO</li>
-		<li class="portfolio-filter">Consumer</li>
-		<li class="portfolio-filter">Automotive</li>
-		<li class="portfolio-filter">Pharma</li>
-		<li class="portfolio-filter">B2B</li>
-		<li class="portfolio-filter">Insurance</li>
-		<li class="portfolio-filter">Financial Services</li>
-		<li class="portfolio-filter">Healthcare</li>
+  <li class="portfolio-filter active">All</li>
+  {props.ecotypes.map(course => {
+          return (
+            <React.Fragment>
+              <li class="portfolio-filter ">{course.node.data.name.text}</li>
+            </React.Fragment>
+          )
+        })}
 	</ul>
   <div class="filter-block-wrap">
-  <div class="filter-block-content portfolio-filter-block">
-    <img src="https://1c56ea1o8ppe3ily2evtgp8h-wpengine.netdna-ssl.com/wp-content/uploads/2019/05/Mautic-Logo.png" alt=""/>
-  </div>
-  <div class="filter-block-content portfolio-filter-block">
-    <img src="https://1c56ea1o8ppe3ily2evtgp8h-wpengine.netdna-ssl.com/wp-content/uploads/2019/05/Hubspot-Logo.png" alt=""/>
-  </div>
-  <div class="filter-block-content portfolio-filter-block">
-    <img src="https://1c56ea1o8ppe3ily2evtgp8h-wpengine.netdna-ssl.com/wp-content/uploads/2019/05/Marketo-Logo-circle.png" alt=""/>
-  </div>
-  <div class="filter-block-content portfolio-filter-block">
-    <img src="https://1c56ea1o8ppe3ily2evtgp8h-wpengine.netdna-ssl.com/wp-content/uploads/2019/05/Pardot-Logo.png" alt=""/>
-  </div>
-  <div class="filter-block-content portfolio-filter-block">
-    <img src="https://1c56ea1o8ppe3ily2evtgp8h-wpengine.netdna-ssl.com/wp-content/uploads/2019/05/Mailchimp-Logo.png" alt=""/>
-  </div>
-  <div class="filter-block-content portfolio-filter-block">
-    <img src="https://1c56ea1o8ppe3ily2evtgp8h-wpengine.netdna-ssl.com/wp-content/uploads/2019/05/Adobe-campaign-logo.png" alt=""/>
-  </div>
-  
-  
-  <div class="filter-block-content portfolio-filter-block">
-    <img src="https://1c56ea1o8ppe3ily2evtgp8h-wpengine.netdna-ssl.com/wp-content/uploads/2019/05/Mautic-Logo.png" alt=""/>
-  </div>
+  {props.ecosys.map(ecosystem => {
+          return (
+            <React.Fragment>
+              <div class="filter-block-content portfolio-filter-block">
+                 <img src={ecosystem.node.data.logo.url} alt=""/>
+               </div>
+            </React.Fragment>
+          )
+        })}
+ 
   <div class="filter-block-content portfolio-filter-block">
     <img src="https://1c56ea1o8ppe3ily2evtgp8h-wpengine.netdna-ssl.com/wp-content/uploads/2019/05/Hubspot-Logo.png" alt=""/>
   </div>
@@ -62,7 +41,10 @@ const Ecosystem = () => (
   <div class="filter-block-content portfolio-filter-block">
     <img src="https://1c56ea1o8ppe3ily2evtgp8h-wpengine.netdna-ssl.com/wp-content/uploads/2019/05/Adobe-campaign-logo.png" alt=""/>
   </div>
+ 
 </div></div>
 )
 
 export default Ecosystem
+
+ 
--- src/templates/post.jsx
+++ src/templates/post.jsx
@@ -3,14 +3,15 @@ import { graphql } from "gatsby"
 import Header from "../components/header"
 import Ecosystem from "../components/ecosystem"
 
-const Post = ({ data: { prismicAgency } }) => {
+const Post = ({ data: { prismicAgency, allPrismicEcosystemType, allPrismicEcosystem } }) => {
   const { data } = prismicAgency
+  const ecotypes = allPrismicEcosystemType.edges
+  const ecosys = allPrismicEcosystem.edges
   return (
     <React.Fragment>
     	<Header />
-     
-      <Ecosystem />
-    </React.Fragment>
+      <Ecosystem ecotypes = {ecotypes}  ecosys = {ecosys}/>
+      </React.Fragment>
   )
 }
 
@@ -19,17 +20,46 @@ export default Post
 export const pageQuery = graphql`
   query PostBySlug($uid: String!) {
     prismicAgency(uid: {eq: $uid}) {
-    data {
-      hero_title {
-        text
-      }
-      hero_des {
-        text
-      }
-      hero_image {
-        url
+      data {
+        hero_title {
+          text
+        }
+        hero_des {
+          text
+        }
+        hero_image {
+          url
+        }
       }
     }
-  }
+    allPrismicEcosystemType {
+      edges {
+        node {
+          id
+          data {
+            name {
+              text
+            }
+          }
+        }
+      }
+    }
+    allPrismicEcosystem {
+      edges {
+        node {
+          data {
+            logo {
+              url
+            }
+            name {
+              text
+            }
+            type {
+              slug
+            }
+          }
+        }
+      }
+    }
   }
 `
